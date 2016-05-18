a62ebcc117e64de7e44188a83e791f00097fc877
Tim <tim.debruyne@hotmail.com>
2016-05-18 21:14:09 +0200

    Edited Delete method, Updated spring to 4.2.4, Added @Crossorigin annotation

--- src/main/java/internetprogrammeren/gameprojectrestapi/controller/GameProjectRestController.java
+++ src/main/java/internetprogrammeren/gameprojectrestapi/controller/GameProjectRestController.java
@@ -10,6 +10,8 @@ import domain.ServiceFacade;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,27 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Tim
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/gameapi")
 public class GameProjectRestController{
     @Autowired
     public ServiceFacade service;
     
+    @CrossOrigin
     @RequestMapping(method=RequestMethod.POST,  consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addGameRest(@RequestBody Game game){
         service.addGame(game);
     }
     
+    @CrossOrigin
     @RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
     public Collection<Game> getGamesRest(){
         return service.getAllGames();
     }
     
-    @RequestMapping(value="/delete", method=RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void deleteGame(@RequestBody Game game){
-        service.removeGame(game);
+    @CrossOrigin
+    @RequestMapping(value="/delete/{id}")
+    public void deleteGame(@PathVariable int id){
+        service.removeGame(service.getGame(id));
     }
     
+    @CrossOrigin
     @RequestMapping(value="/update", method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
     public void updateGame(@RequestBody Game game){
         service.updateGame(game);
